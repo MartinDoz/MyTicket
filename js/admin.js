3cff88dacf4817d6e2bd95e458bfad91df7c6913
Martin Doz <martindoz777@gmail.com>
2022-11-14 19:08:10 -0300

    delete show y edit show

--- js/admin.js
+++ js/admin.js
@@ -5,17 +5,19 @@ const tableBodyHTML = document.getElementById('table-body');
 const addShowForm = document.getElementById('addShowForm')
 const cardsShow = document.getElementById('cardsShow')
 
+let editable;
+
 let savedShows = JSON.parse(localStorage.getItem('shows-key'))
 
 // if(savedShows === null)
-if(!savedShows) {
+if (!savedShows) {
     savedShows = Shows
 }
 
 function renderShows() {
     console.log("loco")
     tableBodyHTML.innerHTML = '';
-    savedShows.forEach(elem => {
+    savedShows.forEach((elem, index) => {
         tableBodyHTML.innerHTML += `<tr>
                                         <td>
                                             <img class="table-img" src=${elem.image} />
@@ -23,8 +25,16 @@ function renderShows() {
                                             <td>${elem.name}</td>
                                             <td>${elem.dataPlace}</td>
                                             <td>$ ${elem.price}</td>
-                                            <td>${elem.stock}</td>
+                                            <td class="">${elem.stock ? `<i class="fa-solid fa-check"></i>` : `<i class="fa-solid fa-xmark"></i>`}  </td>
+                                            <td>
+                                            <button class="btn btn-danger p-1 me-2" onclick="deleteShow(${index})">
+                                            <i class="fa-solid fa-trash-can"></i>
+                                            </button>
+                                            <button class="btn btn-success p-1" onclick="editShow(${index})"><i
+                                            class="fa-solid fa-pencil"></i> </button>
+                                            </td>
                                             </tr>`
+
     });
 }
 
@@ -48,11 +58,41 @@ addShowForm.addEventListener('submit', (evt) => {
         stock: formEl.stock.checked
     }
 
-    savedShows.push(newShow)
+    if(editable) {
+        savedShows[editable] = newShow;
+    } else {
+        savedShows.push(newShow)
+    }
+
+    editable = undefined; 
 
     localStorage.setItem('shows-key', JSON.stringify(savedShows))
 
     renderShows()
-    
+ 
     addShowForm.reset()
 })
+
+function deleteShow(idx) {
+    savedShows.splice(idx,1);
+    localStorage.setItem('shows-key', JSON.stringify(Shows))
+    renderShows();
+}
+
+function editShow(idx) {
+    const showToEdit = savedShows[idx]
+    
+    const formEl = addShowForm.elements
+
+    formEl.name.value = showToEdit.name;
+    formEl.dataPlace.value = showToEdit.dataPlace;
+    formEl.price.valueAsNumber = showToEdit.price;
+    formEl.image.value = showToEdit.image;
+
+    formEl.stock.checked = showToEdit.stock;
+
+    editable = idx
+    
+}
+
+
